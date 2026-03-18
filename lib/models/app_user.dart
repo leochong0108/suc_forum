29f78cba58810fb709d71a1e9a8d672635cf1cf4
Leo Chong <leochong0108@gmail.com>
2026-03-19 00:21:18 +0800

    create user model

---
+++ lib/models/app_user.dart
@@ -0,0 +1,19 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppUser {
+  final String uid;
+  final String name;
+  final String email;
+  final String role;
+
+  AppUser({
+    required this.uid,
+    required this.name,
+    required this.email,
+    this.role = 'user',
+  });
+
+  Map<String, dynamic> toMap() {
+    return {'uid': uid, 'name': name, 'email': email, 'role': role};
+  }
+}
